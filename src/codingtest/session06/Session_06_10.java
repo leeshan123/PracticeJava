caf0421b11fd596618408df5069d7c7d4d7097ab
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2024-07-05 20:23:56 +0900

    7월5일 코테연습

--- src/codingtest/session06/Session_06_10.java
+++ src/codingtest/session06/Session_06_10.java
@@ -5,51 +5,51 @@ package codingtest.session06;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Session_06_09 {
-        //세션 6번의 8번 문제(뮤직비디오(결정알고리즘))
+public class Session_06_10 {
+        //세션 6번의 10번 문제(마구간 정하기(결정알고리즘))
         //X
 
-        public int count(int[] arr, int capacity){
-
+        public int count(int[] arr, int dist){
             int count = 1;
-            int sum = 0;
+            int ep = arr[0];
 
-            for(int x : arr){
-                if(sum+x > capacity){
+            for(int i=1; i<arr.length; i++){
+                if(arr[i]-ep > dist){
                     count++;
-                    sum = x;
+                    ep = arr[i];
                 }
-                else sum += x;
             }
-
             return count;
+
         }
 
 
         public int solution(int n,int m,int[] arr) {
             int answer = 0;
 
-            //Arrays메서드 잘보자
-            int lt = Arrays.stream(arr).max().getAsInt();
-            int rt = Arrays.stream(arr).sum();
-
+            Arrays.sort(arr);
+            int lt = 1;
+            int rt = arr[n-1];
             while(lt<=rt){
                 int mid = (lt+rt)/2;
-                if(count(arr,mid)<=m){
-                    answer = mid;
-                    rt = mid-1;
-                }
-                else {
-                    lt = mid +1;
-                }
+                if(count(arr,mid)>=m){
+                    answer=mid;
+                    lt = mid+1;
+                }else rt = mid-1;
             }
+
+
+
+
+
+
 
             return answer;
         }
 
 
         public static void main(String[] args) {
-            Session_06_09 T = new Session_06_09();
+            Session_06_10 T = new Session_06_10();
             Scanner scan = new Scanner(System.in);
 
             int n = scan.nextInt();
