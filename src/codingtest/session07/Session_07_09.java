b0516aed49738ae00fb402139773739e57c98e33
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2024-07-23 19:11:37 +0900

    7월 23일 코테 공부 BFS

--- src/codingtest/session07/Session_07_09.java
+++ src/codingtest/session07/Session_07_09.java
@@ -3,6 +3,8 @@ package codingtest.session07;
 
 
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 
@@ -13,14 +15,22 @@ public class Session_07_09 {
 
         Node root;
 
-        public int DFS(int L, Node root){
-            if(root.lt == null && root.rt ==null){
-                return L;
-            }else {
-                return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
+        public int BFS( Node root){
+            Queue<Node> Q = new LinkedList<>();
+            Q.offer(root);
+            int L = 0;
+            while(!Q.isEmpty()){
+                int len = Q.size();
+                for(int i=0; i<len; i++){
+                    Node cur = Q.poll();
+                    if(cur.lt == null && cur.rt == null) return L;
+                    if(cur.lt!= null) Q.offer(cur.lt);
+                    if(cur.rt!= null) Q.offer(cur.rt);
+                }
+                L++;
             }
 
-
+            return 0;
         }
 
 
@@ -34,7 +44,7 @@ public class Session_07_09 {
             tree.root.rt = new Node(3);
             tree.root.lt.lt = new Node(4);
             tree.root.lt.rt = new Node(5);
-            System.out.println(tree.DFS(0,tree.root));
+            System.out.println(tree.BFS(tree.root));
 
 
         }
---
+++ src/codingtest/session07/Session_07_10.java
@@ -0,0 +1,55 @@
+package codingtest.session07;
+
+
+
+
+import java.util.Scanner;
+
+
+public class Session_07_10 {
+        //9. Tree 말단노드까지의 까장 짧은 경로(DFS)
+        //문제에 없음
+
+
+        Node root;
+
+        public int DFS(int L, Node root){
+            if(root.lt == null && root.rt ==null){
+                return L;
+            }else {
+                return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
+            }
+
+
+        }
+
+
+
+        public static void main(String[] args) {
+            Session_07_10 tree = new Session_07_10();
+            Scanner scan = new Scanner(System.in);
+
+            tree.root = new Node(1);
+            tree.root.lt = new Node(2);
+            tree.root.rt = new Node(3);
+            tree.root.lt.lt = new Node(4);
+            tree.root.lt.rt = new Node(5);
+            System.out.println(tree.DFS(0,tree.root));
+
+
+        }
+
+    static class Node{
+        int data;
+        Node lt, rt;
+        public Node(int val){
+            data = val;
+            lt=rt=null;
+        }
+    }
+
+
+}
+
+
+
